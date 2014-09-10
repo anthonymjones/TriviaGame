ab960dc9ac58706efaba150ec2954358fa9229ba
anthonymjones <anthony.mjones@gmail.com>
2014-09-10 12:29:54 -0600

    v1

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -14,11 +14,51 @@ namespace TriviaGame
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
 
+            int incorrect = 0;
+            int correct = 0;
+            int questionAsk = 10;
+            Console.WriteLine(@"Welcome to C# Trivia!
+I'll dish out 10 trivia questions and you type the answer and press enter.
+Are you ready? (press enter)");
+            Console.ReadKey();
+
+            int number = 1;
+            while (questionAsk > 0)
+            {
+                Random rng = new Random();
+                int ranNum = rng.Next(0, 5001);
+                var questNum = GetTriviaList()[ranNum];
+
+                Console.WriteLine(questNum.Question);
+                var userAnswer = Console.ReadLine().ToLower();
+
+                if (userAnswer == questNum.Answer.ToLower())
+                {
+                    Console.WriteLine("You got it!");
+                    questionAsk--;
+                    number++;
+                    correct++;
+                    Console.WriteLine("The answer is: {0}", questNum.Answer);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                    questionAsk--;
+                    number++;
+                    incorrect++;
+                    Console.WriteLine("The answer is: {0}", questNum.Answer);
+
+                }
+                
+            }
+            Console.WriteLine("You got {0}/10", correct, questionAsk, "right!" );
+            Console.WriteLine("You got {0}/10", incorrect, questionAsk, "wrong.");
+
             Console.ReadKey();
         }
 
 
-        //This functions gets the full list of trivia questions from the Trivia.txt document
+        //This function gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
         {
             //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
@@ -27,13 +67,19 @@ namespace TriviaGame
             //Each item in list "contents" is now one line of the Trivia.txt document.
             foreach (var item in contents)
             {
-
+                
             }
+            
             //make a new list to return all trivia questions
             List<Trivia> returnList = new List<Trivia>();
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
+            foreach (var question in contents)
+            {
+                Trivia triviaObject = new Trivia(question);
+                returnList.Add(triviaObject);
+            }
             //Return the full list of trivia questions
             return returnList;
         }
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -21,23 +21,18 @@ namespace TriviaGame
         // constructor and assign them to the Question
         // and Answer properties
         //STEP 2. Constructors
-        public Trivia(string question, string answer)
+        public Trivia(string triviaSplit)
         {
-            //set the question
-            this.Question = question;
+
+            List<string> QuestAndAns = new List<string>();
+
+            QuestAndAns = triviaSplit.Split('*').ToList();
+           //set the question
+            this.Question = QuestAndAns[0];
             //set the answer
-            this.Answer = answer;
+            this.Answer = QuestAndAns[1];
         }
-        //STEP 3. Method and/or Functions
-        public string AskAQuestion()
-        {
-            //split the contents at the * and return the 0 index to ask a question
-            return this.Question + " " + "?";
-        }
-        public string GiveAnAnswer()
-        {
-            //split the contents at the * and return the 1 index to give an answer
-            return this.Answer;
+        
         }
     }
-}
+
